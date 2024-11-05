064727f26cfa918e5a2be731ed1ec7f3a98d9666
kcumbie <keith.cumbie@gmail.com>
2024-11-04 19:59:39 -0600

    Add files via upload
    Hybrid Domain join Script

---
+++ HybridAADJ.ps1
@@ -0,0 +1,3 @@
+﻿REG.EXE ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\CDJ\AAD" /v TenantId /t REG_SZ /d TenetID /f
+REG.EXE ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\CDJ\AAD" /v TenantName /t REG_SZ /d Tenant.onmicrosoft.com /f
+REG.EXE ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WorkplaceJoin" /v autoWorkplaceJoin /t REG_DWORD /d 1 /f
